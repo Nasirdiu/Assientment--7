133392824f501ff7c3a554215aa15ced83a666f7
nasirdiu <nasir.joypurhat@gmail.com>
2022-03-10 21:53:23 +0600

    red button done

--- js/app.js
+++ js/app.js
@@ -16,11 +16,13 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    likedPostsId.push(id);
     showPosts(posts);
+
 };
 
 const reportPost = (id) => {
+ 
     reportedPostsId.push(id);
     const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
     showPosts(remainingPosts);
